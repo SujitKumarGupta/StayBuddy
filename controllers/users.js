7d7088f9d472ff8eb5b36c51bdb7377679c15c28
SujitKumarGupta <sujitagrahari555@gmail.com>
2025-07-31 00:24:52 +0530

    Added styling

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,13 @@
 
-const User = require("../models/user");module.exports.signup = async (req, res) => {
+const User = require("../models/user");
+module.exports.signup = async (req, res) => {
     try {
       let { username, email, password } = req.body;
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
       console.log(registeredUser);
+      console.log(registeredUser.username);
+      
       req.login(registeredUser, (err)=>{
         if(err){
           return next(err);
